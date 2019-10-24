0b50c4ae86c94c1bfa5754321cac44fdd347356c
Michael Morgan <michael.gamel@hotmail.com>
2019-10-24 21:31:13 +0400

    dummy API call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-netlify';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get('https://mina-app.herokuapp.com/api')
+      .subscribe(data => console.log);
+  }
 }
